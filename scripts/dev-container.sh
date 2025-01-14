2c95c912f5511edbb1f1082e7317fe71cb955959
gwen31792 <gwen31792@gmail.com>
2025-01-14 09:33:14 +0800

    chore: 更新开发容器配置，添加 Git 配置脚本

---
+++ scripts/dev-container.sh
@@ -0,0 +1,4 @@
+git config --global user.email gwen31792@gmail.com
+git config --global user.name gwen31792
+git remote set-url origin git@github-gwen:gwen31792/ehentai-toplist-archive.git
+pnpm install
